36c56be6106acb248f2f2ded010499178c5e93aa
Abe Kasper <abe.kasper@hva.nl>
2022-03-18 01:47:03 +0100

    trying stuff

--- src/visualization/shapes_mapping.py
+++ src/visualization/shapes_mapping.py
@@ -1,11 +1,15 @@
 import geopandas as gpd
 import matplotlib.pyplot as plt
 
-df = gpd.read_file(r'data\processed\taxi_zones\taxi_zones.shp')
-df.head()
+def plottingNYC():
+    df = gpd.read_file(r'\data\processed\taxi_zones\taxi_zones.shp')
+    df.head()
 
-df.shape
+    df.shape
 
-fig, ax = plt.subplots(1, 1, figsize=(15, 15))
-df.plot(ax=ax, column='borough', legend=True)
-plt.show()
+    fig, ax = plt.subplots(1, 1, figsize=(15, 15))
+    df.plot(ax=ax, column='borough', legend=True)
+    plt.show()
+
+def my_function():
+  print("Hello from a function")
