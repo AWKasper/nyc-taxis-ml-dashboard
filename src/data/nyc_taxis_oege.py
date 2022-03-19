b453e724383892a7fc6eeaa999ca69d51de74edf
Abe Kasper <abe.kasper@hva.nl>
2022-03-20 00:14:08 +0100

    it should work now

--- src/data/nyc_taxis_oege.py
+++ src/data/nyc_taxis_oege.py
@@ -5,6 +5,6 @@ def oege_engine():
     engine = create_engine('mysql+mysqlconnector://kaspera1:H1c3VA29xnjPrT@oege.ie.hva.nl/zkaspera1', echo=True)
     return engine
 
-df = pd.read_sql("SELECT pickup_longitude, pickup_latitude, dropoff_longitude, dropoff_latitude, trip_distance FROM uncleaned_NYC_yellowcabs_2015", oege_engine())
+chunks = pd.read_sql("SELECT pickup_longitude, pickup_latitude, dropoff_longitude, dropoff_latitude, trip_distance FROM uncleaned_NYC_yellowcabs_2015", oege_engine(), chunksize=50000)
 
-df.describe()
+df = pd.concat(chunks, ignore_index=True)
