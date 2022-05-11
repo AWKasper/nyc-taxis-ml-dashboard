9d99deb29724ad2cd8afd02a2757e45811de68df
Abe Kasper <abe.kasper@hva.nl>
2022-05-11 02:05:11 +0200

    name main

--- src/data/nyc_taxis_oege.py
+++ src/data/nyc_taxis_oege.py
@@ -1,24 +1,30 @@
 import pandas as pd
 from sqlalchemy import create_engine
+import matplotlib.pyplot as plt
 
 def oege_engine():
     engine = create_engine('mysql+mysqlconnector://kaspera1:H1c3VA29xnjPrT@oege.ie.hva.nl/zkaspera1', echo=True)
     return engine
 
-chunks = pd.read_sql("SELECT pickup_longitude, pickup_latitude, dropoff_longitude, dropoff_latitude, trip_distance FROM uncleaned_NYC_yellowcabs_2015 LIMIT 1000", oege_engine(), chunksize=50000)
+def main():
 
-df = pd.concat(chunks, ignore_index=True)
+    chunks = pd.read_sql("SELECT * FROM uncleaned_NYC_yellowcabs_2015 LIMIT 100000", oege_engine(), chunksize=50000)
 
-df.describe()
+    df = pd.concat(chunks, ignore_index=True)
 
-df.to_csv(r'local\taxi_coords.csv', index=False)
+    df.describe()
 
-from src.visualization.shapes_mapping import *
+    df.to_csv(r'local\taxi_coords.csv', index=False)
 
-nycdf = plotting_map(r'data\processed\taxi_zones\taxi_zones.shp', 'borough')
+    from src.visualization.shapes_mapping import plotting_map
+
+    nycdf = plotting_map(r'data\processed\taxi_zones\taxi_zones.shp', 'borough')
 
 
-for row in df.itertuples():
-    plt.plot()
+    for row in df.itertuples():
+        plt.plot()
 
-plt.show()
+    plt.show()
+
+if __name__ == '__main__':
+    main()
