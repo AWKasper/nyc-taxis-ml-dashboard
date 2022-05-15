b0e71d68043ed4d4268f9a9d115968c020f2db35
Abe Kasper <abe.kasper@hva.nl>
2022-05-15 21:18:52 +0200

    some simple plots

--- src/models/predicting_amount_rides_phour.py
+++ src/models/predicting_amount_rides_phour.py
@@ -4,6 +4,9 @@ from datetime import datetime
 
 from src.data.nyc_taxis_oege import oege_engine
 
+import seaborn as sns
+import matplotlib.pyplot as plt
+
 # setup of the data that will be used
 
 # firstly getting the amount of trips for every hour
@@ -64,3 +67,24 @@ dfr['time_of_day'] = dfr['tpep_pickup_datetime'].apply(lambda x: x.strftime(r'%H
 dfr.to_sql(name='rides_per_day_2015', con=oege_engine(), if_exists = 'replace', index=False, chunksize=10000)
 
 # now some plots to get a simple view of the data
+
+chunks = pd.read_sql("SELECT * FROM rides_per_day_2015", oege_engine(), chunksize=10000)
+
+dfv = pd.concat(chunks, ignore_index=True)
+
+sns.set_theme(style="ticks")
+
+hue_order = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
+
+sns.relplot(x='time_of_day', y='count', data=dfv, hue='day_of_the_week', kind="line", palette=sns.color_palette("rocket_r", n_colors=7), ci=None, size="month_of_the_year", hue_order=hue_order, alpha=.7)
+
+plt.show() 
+
+sns.relplot(x='mean_temperatureC', y='count', data=dfv, kind="line", ci=None)
+
+plt.show() 
+
+
+
+# training model
+
