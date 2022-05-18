de7c43bd5cc37360012e54296c656ee6c350249e
blokk4 <koen.blok@hva.nl>
2022-05-18 16:31:32 +0200

    fixed path to taxi zones shapes

--- src/visualization/kmeans_map.py
+++ src/visualization/kmeans_map.py
@@ -7,11 +7,10 @@ from sqlalchemy import create_engine
 from sklearn.cluster import KMeans
 import matplotlib.pyplot as plt
 
-def make_kmeans_map_graph(kmeans_points : int = 10):
-    mycwd = os.getcwd()
+def make_kmeans_map_graph(kmeans_points : int = 10, n_data : int = 25000):
     engine = create_engine('mysql+mysqlconnector://kaspera1:H1c3VA29xnjPrT@oege.ie.hva.nl/zkaspera1')
 
-    df = pd.read_sql("SELECT dropoff_longitude, dropoff_latitude FROM uncleaned_NYC_yellowcabs_2015 LIMIT 25000", engine)
+    df = pd.read_sql(f"SELECT dropoff_longitude, dropoff_latitude FROM uncleaned_NYC_yellowcabs_2015 LIMIT {n_data}", engine)
 
     df_fixed = df['dropoff_longitude'].between(-74.3,-73.7) & df['dropoff_latitude'].between(40.0,41.0)
     df = df[df_fixed]
@@ -19,14 +18,10 @@ def make_kmeans_map_graph(kmeans_points : int = 10):
 
     model = KMeans(n_clusters = kmeans_points)
     model.fit(df_num)
-    # print(model.cluster_centers_)
 
     # get plotting map and scatter cluster points
-    p = plotting_map(mycwd  + r'\data\processed\taxi_zones\taxi_zones.shp', 'borough')
+    p = plotting_map(r'..\..\data\processed\taxi_zones\taxi_zones.shp', 'borough')
     p.scatter(df['dropoff_longitude'],df['dropoff_latitude'],alpha=0.05)
     p.scatter(model.cluster_centers_[:,0],model.cluster_centers_[:,1],color='red')
 
-    #Going back to the working directory from before
-    os.chdir(fr'{mycwd}')
-
-    p.show()
+    return p
