c1464515101baf0671c481cc8afa505bdee31e63
blokk4 <koen.blok@hva.nl>
2022-05-26 17:17:56 +0200

    added basics of writing points to string

--- src/visualization/kmeans_data_sorting.py
+++ src/visualization/kmeans_data_sorting.py
@@ -28,9 +28,26 @@ def __write_to_file(filename : str, data : dict):
     with open(filename, 'w') as f:
         json.dump(data, f, indent=4)
 
+def __write_to_db(data : dict):
+    #engine = create_engine('mysql+mysqlconnector://kaspera1:H1c3VA29xnjPrT@oege.ie.hva.nl/zkaspera1')
+    dic = dict()
+    for key in data:
+        point_string = ""
+        for point in data[key]:
+            point_string += f"{point[0]} {point[1]} "
+        dic[key] = point_string
+    
+    for key in dic:
+        points = dic[key].split()
+        print(key)
+        for p in points:
+            print(float(p), end=' ')
+        print()
+
 if __name__ == '__main__':
-    df = __get_data(25)
+    df = __get_data(50)
     df = __format_date_strings(df)
     dic = __order_coords_by_date(df)
-    __write_to_file("rides.json", dic)
+    __write_to_db(dic)
+    #__write_to_file("rides.json", dic)
     
