c378d3843903b358023ab2dc515cb5b64550ad69
blokk4 <koen.blok@hva.nl>
2022-05-28 16:09:09 +0200

    added global constant description

--- src/visualization/kmeans_map.py
+++ src/visualization/kmeans_map.py
@@ -7,6 +7,9 @@ from sqlalchemy import create_engine
 from sklearn.cluster import KMeans
 import matplotlib.pyplot as plt
 
+PLOT_DESCRIPTION = """Blue dots signify a pickup point, the greater the intensity the more saturated the location.
+Red dots indicate centers of the clusters of points. The amount of red dots can be set with the slider on the left."""
+
 def make_kmeans_map_graph(kmeans_points : int = 10, n_data : int = 25000):
     engine = create_engine('mysql+mysqlconnector://kaspera1:H1c3VA29xnjPrT@oege.ie.hva.nl/zkaspera1')
 
@@ -20,8 +23,8 @@ def make_kmeans_map_graph(kmeans_points : int = 10, n_data : int = 25000):
     model.fit(df_num)
 
     # get plotting map and scatter cluster points
-    p = plotting_map(r'..\..\data\processed\taxi_zones\taxi_zones.shp', 'borough')
-    p.scatter(df['dropoff_longitude'],df['dropoff_latitude'],alpha=0.05)
+    p = plotting_map(rf'{os.getcwd()}\data\processed\taxi_zones\taxi_zones.shp', 'borough')
+    p.scatter(df['dropoff_longitude'],df['dropoff_latitude'],alpha=0.05, color='green')
     p.scatter(model.cluster_centers_[:,0],model.cluster_centers_[:,1],color='red')
 
     return p
