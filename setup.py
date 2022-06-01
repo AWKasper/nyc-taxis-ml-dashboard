c90c356eedf4e5466f441080d4aa7d3f5f63ce44
Mika Bontes <mika.bontes@hva.nl>
2022-06-01 10:21:41 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,5 +4,5 @@ setup(name='BigData',
       version='1.0',
       description='Algorithm for New York Cabs',
       author='Project Big Data - T1',
-      packages=['seaborn', 'geopandas', 'streamlit', 'pandas', 'SQLAlchemy', 'numpy', 'sklearn', 'plotly' 'matplotlib'],
+      packages=['seaborn', 'geopandas', 'streamlit', 'pandas', 'SQLAlchemy', 'numpy', 'sklearn', 'plotly', 'matplotlib'],
      )
